13569f864d06530fbaef8798cee1aaf8debf4113
鍋山尚希 <gachapin5689@gmail.com>
2021-01-14 18:14:02 +0900

    first commit

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "gachapin";
+		System.out.println(str);
+
+		str = "mukku";
+		System.out.println(str);
+
+		final int NUM = 7;
+		System.out.println(NUM);
+	}
+}
---
+++ Chapter05.java
@@ -0,0 +1,20 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l =123456789L;
+		System.out.println(l);
+		double d = 100.1;
+		System.out.println(d);
+		boolean b = false;
+		System.out.println(b);
+
+		Float f = 123.456f;
+		System.out.println(f);
+		Integer i = 300;
+		System.out.println(i);
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.print(array[3]);
+	}
+}
