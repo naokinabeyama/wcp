a2504c583a4110a9ae5715abe53e6be3cc053c8a
鍋山尚希 <gachapin5689@gmail.com>
2021-01-29 21:48:22 +0900

    20210129

--- Main.java
+++ Main.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("名前：");
-    String firstName = scanner.next();
-    System.out.print("名字：");
-    String lastName = scanner.next();
-    System.out.print("年齢：");
-    int age = scanner.nextInt();
-    System.out.print("身長(m)：");
-    double height = scanner.nextDouble();
-    System.out.print("体重(kg)：");
-    double weiht = scanner.nextDouble();
 
-    System.out.println("名前は" + firstName + " " + lastName + "です");
-    System.out.println("年齢は" + age + "歳です");
-    if (age >= 20) {
-      System.out.println("成年者です");
-    } else {
-      System.out.println("未成年者です");
+    System.out.print("何人分の情報を入力しますか：");
+    int n = scanner.nextInt();
+    for (int i = 0; i < n; i++) {
+        System.out.println(i + "人目");
+
+        System.out.print("名前：");
+        String firstName = scanner.next();
+
+        System.out.print("名字：");
+        String lastName = scanner.next();
+
+        System.out.print("年齢：");
+        int age = scanner.nextInt();
+
+        System.out.print("身長(m)：");
+        double height = scanner.nextDouble();
+
+        System.out.print("体重(kg)：");
+        double weiht = scanner.nextDouble();
+
+        Person.printData(firstName, lastName, age, height, weiht);
     }
-    System.out.println("身長は" + height + "mです");
-    System.out.println("体重は" + weiht + "kgです");
   }
 }
