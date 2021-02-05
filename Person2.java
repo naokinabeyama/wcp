42ace9cbb9e4fec1a052af561058ec7563881bf6
鍋山尚希 <gachapin5689@gmail.com>
2021-02-05 17:47:03 +0900

    20210205

--- Person2.java
+++ Person2.java
@@ -1,26 +1,56 @@
 class Person {
-	public static int count = 0;
-	public String firstName, lastName;
-	public int age;
-	public double height, weight;
+	private static int count = 0;
+	private String firstName, lastName;
+	private String middleName;
+	private int age;
+	private double height, weight;
+	private String job;
 
-	Person(String firstName, String lastName, int age, double height, double weight) {
+	Person(String firstName, String middleName, String lastName, int age, double height, double weight, String job) {
 		Person.count++;
 		this.firstName = firstName;
+		this.middleName = middleName;
 		this.lastName = lastName;
 		this.age = age;
 		this.height = height;
 		this.weight = weight;
+		this.job = job;
+	}
+
+	Person(String firstName, String middleName, String lastName, int age, double height, double weight, String job) {
+		this(firstName, lastName, age, height, weight, job);
+		this.middleName = middleName;
+	}
+
+	public String getMiddleName() {
+		return this.middleName;
+	}
+
+	public String getJob() {
+		return this.job;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
 	}
 
 	public String fullName() {
-		return this.firstName + " " + this.lastName;
+		if (this.middleName == null) {
+			return this.firstName + " " + this.lastName;
+		} else {
+			return this.firstName + " " + this.middleName + " " +this.lastName;
+		}
 	}
 
 	public void printData() {
 		System.out.println("私の名前は" + this.fullName() + "です");
 		System.out.println("年齢は" + this.age + "歳です");
 		System.out.println("BMIは" + Math.round(this.bmi()) + "です");
+		System.out.println("仕事は" + this.job +"です");
 	}
 
 	public double bmi() {
