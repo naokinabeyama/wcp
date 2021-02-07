b0db371261d15b020d71b5450735765d5497e04e
鍋山尚希 <gachapin5689@gmail.com>
2021-02-07 18:41:06 +0900

    20210207

---
+++ Main3.java
@@ -0,0 +1,31 @@
+importv java.util.Scanner;
+
+class Main3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+
+		System.out.println("【自転車の情報】");
+		bicycle.printData();
+
+		System.out.println("-----------------");
+		System.out.print("走る距離を入力してください：");
+		int bicycleDistance = scanner.nextInt();
+		bicycle.run(bicycle.Distance);
+
+		System.out.println("=================");
+		Car car = new Car("フェラーリ", "赤");
+		System.out.println("【車の情報】");
+		car.printData();
+
+		System.out.println("-----------------");
+		System.out.print("走る距離を入力してください：");
+		int carDistance = scanner.nextInt();
+		car.run(carDistance);
+
+		System.out.println("-----------------");
+		System.out.print("給油する量を入力してください：");
+		int litre = scanner.nextInt();
+		car.change(litre);
+	}
+}
